60978b71d11f41f2123daf69655bdeaa78434317
Evgeni-Ivanov <evgeniivanov.bg@gmail.com>
2018-07-17 19:40:48 +0100

    Add files via upload

---
+++ 01 - First Steps In Coding/Exercises/05. Rectangle Area/05. Rectangle Area.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            var rectangleArea = a * b;
+
+            Console.WriteLine(rectangleArea);
+        }
+    }
+}
