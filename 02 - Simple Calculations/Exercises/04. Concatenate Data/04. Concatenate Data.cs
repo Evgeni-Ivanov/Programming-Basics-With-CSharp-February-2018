91b1e0e7e8b244371fbf146e70389cae1759ee6a
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-05-17 23:28:32 +0100

    Add Problem 04. Concatenate Data
    Add lecture 02 - Simple Calculations - Exercises: Problem 04. Concatenate Data

---
+++ 02 - Simple Calculations/Exercises/04. Concatenate Data/04. Concatenate Data.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Concatenate_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("Enter your first name: ");
+            var firstName = Console.ReadLine();
+
+            // Console.Write("Enter your last name: ");
+            var lastName = Console.ReadLine();
+
+            // Console.Write("Enter your age: ");
+            var age = int.Parse(Console.ReadLine());
+
+            // Console.Write("Enter your town: ");
+            var town = Console.ReadLine();
+
+            var result = ($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
+            Console.WriteLine(result);
+        }
+    }
+}
