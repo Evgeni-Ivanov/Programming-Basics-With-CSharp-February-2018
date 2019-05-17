3294cad88ffa99ed3a9855c3cf3d6e2d8f7339fa
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-05-17 23:29:55 +0100

    Add Problem 05. Trapezoid Area
    Add lecture 02 - Simple Calculations - Exercises: Problem 05. Trapezoid Area

---
+++ 02 - Simple Calculations/Exercises/05. Trapezoid Area/05. Trapezoid Area.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Trapezoid_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter b1: ");
+            var b1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter b2: ");
+            var b2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter h: ");
+            var h = double.Parse(Console.ReadLine());
+
+            var area = (b1 + b2) * h / 2;
+
+            Console.WriteLine("Trapezoid Area = " + area);
+        }
+    }
+}
